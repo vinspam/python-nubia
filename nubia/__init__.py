vinspam/python-nubia

commit 96259c6fb8c92b8955302b3a4bb65a19938ed84e

Allow command & subcommand approximate matching for interactive mode
Summary:
Extended the approximate matching functionality for interactive mode in the following ways:
- Extended approximate matching functionality to include subcommands, not just commands
- It no longer just prints suggestions, but executes in some cases:
  - if there is a single prefix suggestion (unique prefix match) it automatically executes it
  - if there are multiple prefix suggestions, it prints a message with the suggestions
  - if there are no prefix suggestions, and there's a single levenshtein suggestion it automatically executes it
  - if there are multiple levenshtein suggestions, it prints a message with the suggestions
- Added flag to control opt-out behaviour for auto-execution
- Added unit test coverage for the new functionality

Reviewed By: AndreasBackx

Differential Revision: D22865251

fbshipit-source-id: fecd3ca9b72e9d04f69de19736a9707e75127b34

nubia/__init__.py
@@ -36,4 +36,4 @@ __all__ = [
     "statusbar",
 ]
 
-__version__ = "0.2b3"
+__version__ = "0.2b4"

nubia/internal/cmdbase.py
@@ -19,7 +19,7 @@ from typing import Iterable
 from nubia.internal import parser
 from nubia.internal.completion import AutoCommandCompletion
 from nubia.internal.exceptions import CommandParseError
-from nubia.internal.helpers import function_to_str
+from nubia.internal.helpers import find_approx, function_to_str, suggestions_msg
 from nubia.internal.typing import FunctionInspection, inspect_object
 from nubia.internal.typing.argparse import (
     get_arguments_for_command,
@@ -224,14 +224,38 @@ class AutoCommand(Command):
                         "red",
                     )
                     return 2
+                subcommands = self._get_subcommands()
+
+                if subcommand not in subcommands:
+                    suggestions = find_approx(subcommand, subcommands)
+                    if (
+                        self._options.auto_execute_single_suggestions
+                        and len(suggestions) == 1
+                    ):
+                        print()
+                        cprint(
+                            "Auto-correcting '{}' to '{}'".format(
+                                subcommand, suggestions[0]
+                            ),
+                            "red",
+                            attrs=["bold"],
+                        )
+                        subcommand = suggestions[0]
+                    else:
+                        print()
+                        cprint(
+                            "Invalid sub-command '{}',{}, "
+                            "valid sub-commands: {}".format(
+                                subcommand,
+                                suggestions_msg(suggestions),
+                                ", ".join(self._get_subcommands()),
+                            ),
+                            "red",
+                            attrs=["bold"],
+                        )
+                        return 2
+
                 sub_inspection = self.subcommand_metadata(subcommand)
-                if not sub_inspection:
-                    cprint(
-                        "Invalid sub-command '{}', valid values: "
-                        "{}".format(subcommand, ", ".join(self._get_subcommands())),
-                        "red",
-                    )
-                    return 2
                 instance, remaining_args = self._create_subcommand_obj(args_dict)
                 assert instance
                 args_dict = remaining_args

nubia/internal/helpers.py
@@ -12,8 +12,10 @@ import re
 import signal
 import string
 import subprocess
-
 from collections import namedtuple
+from typing import Iterable, Optional
+
+import jellyfish
 
 
 def add_command_arguments(parser, options):
@@ -175,3 +177,43 @@ def catchall(func, *args):
         func(*args)
     except Exception as e:
         print("Error logging to scuba: {}".format(str(e)))
+
+
+def find_approx(cmd_input: str, cmd_map: Optional[Iterable[str]]) -> Iterable[str]:
+    """Finds the closest command to the passed cmd, this is used in case we
+    cannot find an exact match for the cmd
+    We will use two methods, unique prefix match and levenshtein distance match
+    """
+
+    prefix_suggestions = set()
+    levenshtein_suggestions = {}
+
+    for another_command in cmd_map:
+        if str(another_command).startswith(str(cmd_input).lower()):
+            prefix_suggestions.add(another_command)
+        #  removing single letter levenshtein suggestions
+        #  such as `?`, `q` etc
+        elif len(another_command) > 1:
+            distance = jellyfish.damerau_levenshtein_distance(
+                str(cmd_input).lower(), another_command
+            )
+            if distance <= 2:
+                levenshtein_suggestions.update({another_command: distance})
+
+    if prefix_suggestions:
+        return sorted(prefix_suggestions)
+    else:
+        # sort suggestions by levenshtein distance and then by name
+        return [
+            k
+            for k, _ in sorted(
+                levenshtein_suggestions.items(), key=lambda i: (i[1], i[0])
+            )
+        ]
+
+
+def suggestions_msg(suggestions: Optional[Iterable[str]]) -> str:
+    if not suggestions:
+        return ""
+    else:
+        return f" Did you mean {', '.join(suggestions[:-1])} or {suggestions[-1]}?"

nubia/internal/interactive.py
@@ -7,11 +7,15 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-from typing import List, Tuple, Any
 import logging
 import os
-import sys
+from typing import Any, List, Tuple
 
+from nubia.internal.helpers import catchall, find_approx, suggestions_msg
+from nubia.internal.io.eventbus import Listener
+from nubia.internal.options import Options
+from nubia.internal.ui.lexer import NubiaLexer
+from nubia.internal.ui.style import shell_style
 from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import Completer
@@ -21,14 +25,7 @@ from prompt_toolkit.formatted_text import PygmentsTokens
 from prompt_toolkit.history import FileHistory, InMemoryHistory
 from prompt_toolkit.layout.processors import HighlightMatchingBracketProcessor
 from prompt_toolkit.lexers import PygmentsLexer
-
-from nubia.internal.ui.lexer import NubiaLexer
 from termcolor import cprint
-
-from nubia.internal.helpers import catchall
-from nubia.internal.io.eventbus import Listener
-from nubia.internal.options import Options
-from nubia.internal.ui.style import shell_style
 
 
 def split_command(text):
@@ -60,9 +57,7 @@ class IOLoop(Listener):
         # If EDITOR does not exist, take EMACS
         # if it does, try fit the EMACS/VI pattern using upper
         editor = getattr(
-            EditingMode,
-            os.environ.get("EDITOR", "EMACS").upper(),
-            EditingMode.EMACS,
+            EditingMode, os.environ.get("EDITOR", "EMACS").upper(), EditingMode.EMACS
         )
 
         return PromptSession(
@@ -93,20 +88,33 @@ class IOLoop(Listener):
             return self.evaluate_command(cmd, args, input)
 
     def evaluate_command(self, cmd, args, raw):
-        if cmd not in self._command_registry:
-            print()
-            cprint(
-                "Unknown Command '{}',{} type `help` to see all "
-                "available commands".format(
-                    cmd, self._command_registry.find_approx(cmd)
-                ),
-                "magenta",
-                attrs=["bold"],
-            )
-        else:
-            if args is None:
-                args = ""
+        args = args or ""
+
+        if cmd in self._command_registry:
             cmd_instance = self._command_registry.find_command(cmd)
+        else:
+            suggestions = find_approx(
+                cmd, self._command_registry.get_all_commands_map()
+            )
+            if self._options.auto_execute_single_suggestions and len(suggestions) == 1:
+                print()
+                cprint(
+                    "Auto-correcting '{}' to '{}'".format(cmd, suggestions[0]),
+                    "red",
+                    attrs=["bold"],
+                )
+                cmd_instance = self._command_registry.find_command(suggestions[0])
+            else:
+                print()
+                cprint(
+                    "Unknown Command '{}',{} type `help` to see all "
+                    "available commands".format(cmd, suggestions_msg(suggestions)),
+                    "red",
+                    attrs=["bold"],
+                )
+                cmd_instance = None
+
+        if cmd_instance is not None:
             try:
                 ret = self._blacklist.is_blacklisted(cmd)
                 if ret:
@@ -138,9 +146,7 @@ class IOLoop(Listener):
                 try:
                     text = prompt.prompt(
                         PygmentsTokens(self._get_prompt_tokens()),
-                        rprompt=PygmentsTokens(
-                            self._status_bar.get_rprompt_tokens()
-                        ),
+                        rprompt=PygmentsTokens(self._status_bar.get_rprompt_tokens()),
                     )
                     session_logger = self._plugin.get_session_logger(self._ctx)
                     if session_logger:

tests/helpers_test.py
@@ -10,7 +10,12 @@
 import unittest
 from typing import Dict, List, Optional, Union
 
-from nubia.internal.helpers import catchall, function_to_str
+from nubia.internal.helpers import (
+    catchall,
+    find_approx,
+    function_to_str,
+    suggestions_msg,
+)
 from nubia.internal.typing.inspect import is_optional_type
 
 
@@ -42,9 +47,44 @@ class HelpersTest(unittest.TestCase):
         self.assertRaises(KeyboardInterrupt, catchall, raise_keyboard_interrupt)
         self.assertRaises(SystemExit, catchall, raise_sysexit)
 
+    def test_find_approx(self):
+        commands_map = ["maintenance", "malloc", "move", "list"]
+
+        # check levenshtein approximation
+        self.assertEqual(find_approx("maintenanec", commands_map), ["maintenance"])
+        self.assertEqual(find_approx("ls", commands_map), ["list"])
+
+        # check prefix matching with single result
+        self.assertEqual(find_approx("mal", commands_map), ["malloc"])
+        self.assertEqual(find_approx("maint", commands_map), ["maintenance"])
+
+        # check prefix matching and levenshtein don't generate duplicate suggestions
+        self.assertEqual(find_approx("lis", commands_map), ["list"])
+
+        # check prefix matching with more than one result - should return none
+        self.assertEqual(find_approx("ma", commands_map), ["maintenance", "malloc"])
+        self.assertEqual(
+            find_approx("m", commands_map), ["maintenance", "malloc", "move"]
+        )
+
+        # check no results
+        self.assertEqual(find_approx("a", commands_map), [])
+
     def test_is_optional(self):
         self.assertFalse(is_optional_type(List[str]))
         self.assertFalse(is_optional_type(Dict[str, int]))
         self.assertFalse(is_optional_type(Union[str, int]))
         self.assertTrue(is_optional_type(Optional[str]))
         self.assertTrue(is_optional_type(Union[str, None]))
+
+    def test_suggestions_msg(self):
+        suggestions = []
+        self.assertEqual(suggestions_msg(suggestions), "")
+
+        suggestions = ["one", "two"]
+        self.assertEqual(suggestions_msg(suggestions), " Did you mean one or two?")
+
+        suggestions = ["one", "two", "three"]
+        self.assertEqual(
+            suggestions_msg(suggestions), " Did you mean one, two or three?"
+        )

example/nubia_example.py
@@ -8,9 +8,11 @@
 #
 
 import sys
+
+import example.commands
 from nubia import Nubia, Options
 from nubia_plugin import NubiaExamplePlugin
-import example.commands
+
 
 if __name__ == "__main__":
     plugin = NubiaExamplePlugin()
@@ -18,6 +20,8 @@ if __name__ == "__main__":
         name="nubia_example",
         command_pkgs=example.commands,
         plugin=plugin,
-        options=Options(persistent_history=False),
+        options=Options(
+            persistent_history=False, auto_execute_single_suggestions=False
+        ),
     )
     sys.exit(shell.run())

nubia/internal/options.py
@@ -17,3 +17,15 @@ class Options:
     # File-based history is enabled by default. If this is set to false, we
     # fallback to the in-memory history.
     persistent_history: bool = True
+
+    # Auto-executing single suggestions is enabled by default.
+    #  - if there is a single prefix suggestion (unique prefix match) it automatically
+    #    executes it
+    #  - if there are multiple prefix suggestions, it prints a message with the
+    #    suggestions
+    #  - if there are no prefix suggestions, and there's a single levenshtein
+    #    suggestion it automatically executes it
+    #  - if there are multiple levenshtein suggestions, it prints a message with the
+    #    suggestions
+    # If this is set to false it just prints the suggestions
+    auto_execute_single_suggestions: bool = True

nubia/internal/registry.py
@@ -7,8 +7,6 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-import jellyfish
-
 from nubia.internal.cmdbase import Command
 from nubia.internal.io.eventbus import Listener
 
@@ -105,28 +103,11 @@ class CommandsRegistry:
     def get_all_commands(self):
         return set(self._cmd_instance_map.values())
 
+    def get_all_commands_map(self):
+        return self._cmd_instance_map
+
     def find_command(self, cmd):
         return self._cmd_instance_map.get(cmd.lower())
-
-    def find_approx(self, command) -> str:
-        """Finds the closest command to the passed cmd, this is used in case we
-        cannot find an exact match for the cmd
-        """
-        def are_close_enough(this, that):
-            return jellyfish.damerau_levenshtein_distance(this, that) <= 2
-
-        suggestions = [
-            another_command
-            for another_command in self._cmd_instance_map
-            if are_close_enough(str(command), str(another_command))
-        ]
-
-        if not suggestions:
-            return ""
-        elif len(suggestions) == 1:
-            return f" Did you mean {suggestions[0]}?"
-        else:
-            return f" Did you mean {', '.join(suggestions[:-1])} or {suggestions[-1]}?"
 
     def get_completions(self, document, complete_event):
         return self._completer.get_completions(document, complete_event)

commit a00030972bc26152d0b538687f662a70992ba8e0

Fixing regression with subcommand approximate matching auto-execution
Summary: The functionality implemented in https://github.com/facebookincubator/python-nubia/commit/96259c6fb8c92b8955302b3a4bb65a19938ed84e introduced a regression. This is its fix.

Reviewed By: freewilll

Differential Revision: D23961428

fbshipit-source-id: 206fcdbec4c5faf64723f6c7005b631f007f0a93

nubia/__init__.py
@@ -36,4 +36,4 @@ __all__ = [
     "statusbar",
 ]
 
-__version__ = "0.2b4"
+__version__ = "0.2b5"

nubia/internal/cmdbase.py
@@ -14,12 +14,13 @@ import sys
 import traceback
 from collections import OrderedDict
 from textwrap import dedent
-from typing import Iterable
+from typing import Iterable, Optional
 
 from nubia.internal import parser
 from nubia.internal.completion import AutoCommandCompletion
 from nubia.internal.exceptions import CommandParseError
 from nubia.internal.helpers import find_approx, function_to_str, suggestions_msg
+from nubia.internal.options import Options
 from nubia.internal.typing import FunctionInspection, inspect_object
 from nubia.internal.typing.argparse import (
     get_arguments_for_command,
@@ -138,9 +139,10 @@ class Command:
 
 
 class AutoCommand(Command):
-    def __init__(self, fn):
+    def __init__(self, fn, options: Optional[Options] = None):
         self._built_in = False
         self._fn = fn
+        self._options = options or Options()
 
         if not callable(fn):
             raise ValueError("fn argument must be a callable")
@@ -229,8 +231,8 @@ class AutoCommand(Command):
                 if subcommand not in subcommands:
                     suggestions = find_approx(subcommand, subcommands)
                     if (
-                        self._options.auto_execute_single_suggestions
-                        and len(suggestions) == 1
+                        len(suggestions) == 1
+                        and self._options.auto_execute_single_suggestions
                     ):
                         print()
                         cprint(
@@ -244,7 +246,7 @@ class AutoCommand(Command):
                     else:
                         print()
                         cprint(
-                            "Invalid sub-command '{}',{}, "
+                            "Invalid sub-command '{}'{} "
                             "valid sub-commands: {}".format(
                                 subcommand,
                                 suggestions_msg(suggestions),

nubia/internal/helpers.py
@@ -216,4 +216,4 @@ def suggestions_msg(suggestions: Optional[Iterable[str]]) -> str:
     if not suggestions:
         return ""
     else:
-        return f" Did you mean {', '.join(suggestions[:-1])} or {suggestions[-1]}?"
+        return f", Did you mean {', '.join(suggestions[:-1])} or {suggestions[-1]}?"

nubia/internal/interactive.py
@@ -107,7 +107,7 @@ class IOLoop(Listener):
             else:
                 print()
                 cprint(
-                    "Unknown Command '{}',{} type `help` to see all "
+                    "Unknown Command '{}'{} type `help` to see all "
                     "available commands".format(cmd, suggestions_msg(suggestions)),
                     "red",
                     attrs=["bold"],

nubia/internal/nubia.py
@@ -141,8 +141,9 @@ class Nubia:
             self._command_pkgs = [self._command_pkgs]
         for pkg in self._command_pkgs:
             for cmd in cmdloader.load_commands(pkg):
-                self._registry.register_command(AutoCommand(cmd), override=True)
-
+                self._registry.register_command(
+                    AutoCommand(cmd, self._options), override=True
+                )
         # By default, if we didn't receive any command we will use the connect
         # command which drops us to an interactive mode.
         self._opts_parser.set_default_subparser("connect")

tests/helpers_test.py
@@ -82,9 +82,9 @@ class HelpersTest(unittest.TestCase):
         self.assertEqual(suggestions_msg(suggestions), "")
 
         suggestions = ["one", "two"]
-        self.assertEqual(suggestions_msg(suggestions), " Did you mean one or two?")
+        self.assertEqual(suggestions_msg(suggestions), ", Did you mean one or two?")
 
         suggestions = ["one", "two", "three"]
         self.assertEqual(
-            suggestions_msg(suggestions), " Did you mean one, two or three?"
+            suggestions_msg(suggestions), ", Did you mean one, two or three?"
         )